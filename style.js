a75ec622bca8a83d4c28fd2b8649be15ce89e6a4
usanase Francoise <fusanasencoise@gmail.com>
2019-07-19 17:34:13 +0200

    if commit

--- style.js
+++ style.js
@@ -12,3 +12,8 @@ function akanName(){
  
             var gender='Female';     
             }
+            console.log(gender);
+ var CC=parseInt(YY/100)+1;
+var d=parseInt(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7);
+var d=(d.toFixed(0));
+console.log(d);
