65709339fe604c3c02dfd573ddd99a2bbd104c13
usanase Francoise <fusanasencoise@gmail.com>
2019-07-19 17:35:21 +0200

    if commit

--- style.js
+++ style.js
@@ -17,3 +17,15 @@ function akanName(){
 var d=parseInt(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7);
 var d=(d.toFixed(0));
 console.log(d);
+if(d==0 && gender=='Male'){
+    document.getElementById("boo").innerHTML= " your Akan Name is Kwasi";
+}
+else if(d==1 && gender=='Male'){
+    document.getElementById("boo").innerHTML=" your Akan Name is Kwadwo";
+}
+else if(d==2 && gender=='Male'){
+    document.getElementById("boo").innerHTML=" your Akan Name is Kwabena";
+}
+else if(d==3 && gender=='Male'){
+    document.getElementById("boo").innerHTML=" your Akan Name is Kwaku";
+}
